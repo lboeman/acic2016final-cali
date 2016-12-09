40aa2d0f9efc15fb5315bab824ccadb51679caad
team cali chameleon head <finalcc@teamcali>
2016-12-09 20:51:45 +0000

    dgal and getCSV updates

--- gdalMask.py
+++ gdalMask.py
@@ -1,125 +1,57 @@
-"""
-	Gdal masking script originally written by Kyle Saxberg
-"""
-# Script of gdal comands so far
+# A script for re-projecting lat/lon coordinates written by Kyle Saxberg
+# lots of hard coded thigns in here
 import gdal
 import numpy as np
 
-#Working on these package imports
-import osr
-import ogr
 
-# ----------------------------------------
-# # Simple commands for manipulation of the dataset
-#
-# #Opening the only file in the data which can be opened by GDAL
-# db = gdal.Open("nlcd_2011_landcover_2011_edition_2014_10_10.img")
-# dbBand = db.GetRasterBand(1)
-#    See the statistics: Min, Max, Mean, Stddev 
-#    Negatively useful for our raster band
-# dbBand.GetStatistics(True, True) 
-#
-#
-# db.RasterXSize
-# db.RasterYSize
-# db.RasterCount
-# 
-# #The following fails for the projection type we have
-# geotrans = db.GetGeoTransform()
-# 
-# bandtype = gdal.GetDataTypeName(dbBand.DataType)
-# 
-# # Read raster is the key, we can chose (xOffset, yOffset, xMax, yMax, xBufferSize, yBufferSize, datatype)
-# # Meaning that the need to parse what offset means in the context of lat/long cordinates
-# scaline = dbBand.ReadRaster(0, 0, dbBand.xSize, 1, dbBand.XSize, 1, dbBand.DataType)
-# import struct
-# vals = struct.unpack('b'*dbBand.XSize, scanline)
-# # vals were all zero for first row of raster
-# # Random sample of middle row yielded the appropriate assingment from landcover
-# 
+def convertLatLon(geotrans, lat, lon):
+	px = int((lon - geotrans[0]) / geotrans[1]) # x pixel
+	py = int((lat - geotrans[3]) / geotrans[5]) # y pixel
+	return px, py
 
 
-# ---------------------------------------
-#  The method below was an attempt to convert point querry to the projection the dataset was already in.
-#  This method encountered problems as the ogr module wanted EPSG csv files set to a GDAL_DATA location.
+def getMyMask(dbBand, geotrans, lat, lon):
+	#TODO: fix hardcoding of masking values
+	maskingValues = [21, 22, 23, 24]
+	#TODO: fix assumptions on area questioning, ie threshold
+	# geotrans = (xstart, xstep, ??, ystart, ??, ystep)
+	px, py = convertLatLon(geotrans, lat, lon)
+	
+	# Single point 
+	#intval = dbBand.ReadAsArray(px, py, 1, 1)
+	#isItDevelopedPoint = False
+	#if intval in maskingValues:
+	#	isItDevelopedPoint = True
+	
+	# Over a small region
+	# each single step in mask is ~120ft
+	# this draws a ~ 1000x1000ft box over the lat/lon pair
+	threshold = 30 
+	boxOfInterest = dbBand.ReadAsArray(px-5, py-5, 10, 10)
+	totalCount = 0
+	isItDevelopedArea = False
+	for i in boxOfInterest.flatten():
+		if i in maskingValues:
+			totalCount += 1
+	if totalCount >= threshold:
+		isItDevelopedArea = True
+	return isItDevelopedArea
+	
 
-#  need to convert lat/lon to query projection
-SourceEPSG = 5070
-TargetEPSG = 4326
+if __name__=="__main__":
+	
+	filename = "/home/cc/landCoverDataProjection"
+	db = gdal.Open(filename)
+	gt = db.GetGeoTransform()
+	dbBand = db.GetRasterBand(1)
+	geotrans = db.GetGeoTransform()
 
-source = osr.SpatialReference()
-source.ImportFromEPSG(SourceEPSG)
+	latLons = "/home/acic2016final-cali/CaliforniaIDs.csv"
+	with open(latLons) as f:
+		header = f.readline()
+		print(header.strip())
+		for line in f:
+			lat, lon = [float(x.strip()) for x in line.split(',')]
+			if getMyMask(dbBand, geotrans, lat, lon):
+				print(line.strip())
 
-target = osr.SpatialReference()
-target.ImportFromEPSG(TargetEPSG)
-
-def CRSTransform(Lat, Long):
-    transform = osr.CoordinateTransformation(source, target)
-    #point = ogr.Geometry(ogr.wkbPoint)
-    point = ogr.CreateGeometryFromWkt("POINT ("+Lat+" "+Long+")")
-    #point.setPoint_2D(0, float(Long), float(Lat))
-    point.Transform(transform)
-    print(point.GetX() + "   " + point.getY())
-
-
-
-
-#=========================================================================================
-# Resources:
-
-# associated gdal programs: https://github.com/dwtkns/gdal-cheat-sheet
-
-# BASH STUFF
-# Command used to warp image to the most commonly used lat/lon projection:
-#gdalwarp -s_srs epsg:5070 -t_srs epsg:4326 nlcd_2011_landcover_2011_edition_2014_10_10.img convertedCover
-#    The output file, convertedCover, is able to be loaded into QGIS and appears to have the right coordinates
-#     what remains to be seen is iteration over this new matrix. 
-
-# GDAL raster manipulation
-#https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#get-raster-metadata
-# Looking at creating a Driver for the information, "Converting an OGR file to Raster"
-# Driver may be the key to abritrary iteration here. 
-
-
-
-#=====================================
-# helpful sites are helpful
-# https://gis.stackexchange.com/questions/46893/getting-pixel-value-of-gdal-raster-under-ogr-point-without-numpy
-filename = "convertedThing"
-
-db = gdal.Open(filename)
-gt = db.GetGeoTransform()
-dbBand = db.GetRasterBand(1)
-
-def getMyMask(dbBand, lat, lon):
-    #TODO: Fix hardcoding of masking values
-    #TODO: Fix Assumptions on Area questioning
-    #Geotrans = (xstart, xstep, ???, ystart, ??, ystep)
-    geotrans = db.GetGeoTransform()
-    px, py = convertLatLon(geotrans, lat, lon)
-
-    # Single Point 
-    intval = dbBand.ReadAsArray(px, py, 1, 1)
-    isItDevelopedPoint = False
-    if inval in [21, 22, 23, 24]:
-        isItDevelopedPoint = True
-
-    # Average over an .... area
-    #   each single step in mask is ~ 120ft
-    #   draws 1000x1000 ft box over the lon/lat pair
-    boxOfInterest = dbBand.ReadAsArray(px-5, py-5, 10, 10)
-    totalCount = 0
-    isItDevelopedArea = False
-    for i in boxOfInterest.flatten():
-        if i in [21, 22, 23, 24]:
-            totalCount += 1
-    if totalCount >= 25:
-        isItDevelopedArea = True
-    
-    return isItDevelopedPoint
-
-def convertLatLong(geotransform, lat, lon):
-    px = int((lon - geotransform[0]) / geotransform[1]) # x pixel
-    py = int((lat - geotransform[3]) / geotransform[5]) # y pixel
-    
-    return px, py
--- getCSV.py
+++ getCSV.py
@@ -1,5 +1,7 @@
 """
 Gathers maca data using openDap
+
+TODO: implement argparse rather than sys.argv for cli arguments
 """
 from netCDF4 import Dataset
 from dateutil.parser import parse
@@ -9,7 +11,10 @@ import numpy as np
 import pandas as pd
 import argparse as parse
 import requests
+import sys
+import gdalMask
 from io import StringIO
+
 """
 	1 = lat
 	2 = lon
@@ -21,7 +26,6 @@ from io import StringIO
 	8 = scenario (rcp45 or rcp 85)
 """
 URL = "http://climate-dev.nkn.uidaho.edu/Services/get-netcdf-data/?download-csv=True&request_lat_lon=False&lat={1}&lon={2}&positive-east-longitude=True&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav2metdata_{3}_GFDL-ESM2M_r1i1p1_{8}_{9}_CONUS_daily.nc&variable=air_temperature&variable-name={3}_GFDL-ESM2M_{8}&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav2metdata_{4}_GFDL-ESM2M_r1i1p1_{8}_{9}_CONUS_daily.nc&variable=air_temperature&variable-name={4}_GFDL-ESM2M_{8}&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav2metdata_{5}_GFDL-ESM2M_r1i1p1_{8}_{9}_CONUS_daily.nc&variable=relative_humidity&variable-name={5}_GFDL-ESM2M_{8}&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav2metdata_{6}_GFDL-ESM2M_r1i1p1_{8}_{9}_CONUS_daily.nc&variable=relative_humidity&variable-name={6}_GFDL-ESM2M_{8}&data-path=http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_macav2metdata_{7}_GFDL-ESM2M_r1i1p1_{8}_{9}_CONUS_daily.nc&variable={7}&variable-name=pr_GFDL-ESM2M_{8}"
-	
 
 CSV_HEADER_MAP = {
 				'T_min':'tasmin_GFDL-ESM2M_rcp45(K)',
@@ -44,51 +48,67 @@ CSV_HEADER_MAP = {
 """
 def getUrl(lat, lon, scenario, period):
 	if(historical):
+		scenario = 'historical'
 		period = '1950_2005'
 	else:
 		period = '2006_2099'
-	return URL.format(lat,lon'tasmin','tasmax','rhsmin','rhsmax','pr',scenario,period)
+	return URL.format(lat,lon,'tasmin','tasmax','rhsmin','rhsmax','pr',scenario,period)
 
-main():
-	lat = 34.2
-	lon = { -119.16}#, -119.04,-119.08,-119.12 }
+def main():
+	if len(sys.argv) < 3:
+		print("too few args")
+		sys.exit(1)
+	
+	lat = sys.argv[1]
+	lon = sys.argv[2]
+	
+	"""
+	scenario = sys.arv[3]
+	historical = sys.argv[4]
+	if scenario not in ['rcp45','rcp85']:
+		print("invalid scenario")
+	if historical not in ['True','False']:
+		print('historical must be true of false')
+	"""
 	data_dir = "data"
 	if not os.path.isdir(data_dir):
 		os.mkdir(data_dir)
-	
-	
-	
-		for l in lon:
-		
-		fn = str(lat)+'_'+str(l)+'.csv'
-		print('requesting data...')
-		csv = requests.get(url.format(lat,l))
-		f = open(data_dir+'/'+fn,'w')
-		f.write(csv.text)
-		f.close()
+	scenarios = ['rcp45', 'rcp85']
+	for scen in scenarios:
+		for hisorical in [True, False]:
+			fn = scen_str(lat)+'_'+str(l)+'.csv'
+			if(historical)
+				fn = 'historical_'+fn
+			else
+				fn = 'future_'+fn
+			print('requesting data...')
+			csv = requests.get(url.format(lat, lon, scen, historical))
+			f = open(data_dir+'/'+fn,'w')
+			f.write(csv.text)
+			f.close()
 
-		df = pd.read_csv(data_dir+'/'+fn,header=15)
-		#df = pd.read_csv(StringIO(csv.text), header=15)
-		print(df.columns.values)
-				
-		df["Year"] = [ x[:4] for x in df['yyyy-mm-dd']]
-		df["Month"] = [ x[5:7] for x in df['yyyy-mm-dd']]
-		df["Day"] = [ x[-2:] for x in df['yyyy-mm-dd']]
-		df["T_max"] = df[CSV_HEADER_MAP["T_max"]] - 273.15
-		df["T_min"] = df[CSV_HEADER_MAP["T_min"]] - 273.15
+			df = pd.read_csv(data_dir+'/'+fn,header=15)
+			#df = pd.read_csv(StringIO(csv.text), header=15)
+			print(df.columns.values)
+					
+			df["Year"] = [ x[:4] for x in df['yyyy-mm-dd']]
+			df["Month"] = [ x[5:7] for x in df['yyyy-mm-dd']]
+			df["Day"] = [ x[-2:] for x in df['yyyy-mm-dd']]
+			df["T_max"] = df[CSV_HEADER_MAP["T_max"]] - 273.15
+			df["T_min"] = df[CSV_HEADER_MAP["T_min"]] - 273.15
 
-		# calculate precipitation in cm and ave relative humidity.
-		df["rh_ave"] = df[[CSV_HEADER_MAP['rhmax'], CSV_HEADER_MAP['rhmin']]].mean(axis=1)
-		df["Precip(cm)"] = df[CSV_HEADER_MAP["Precip(mm)"]].multiply(10)
-		df["T_ave"] = df[[CSV_HEADER_MAP['T_max'],CSV_HEADER_MAP['T_min']]].mean(axis=1)
-		# remove unwanted data
-		del df[CSV_HEADER_MAP["rhmax"]]
-		del df[CSV_HEADER_MAP["rhmin"]]
-		del df["yyyy-mm-dd"]
-		
-		# rename columns that we dont need to alter
-		df['Precip(mm)'] = df[CSV_HEADER_MAP['Precip(mm)']]
-		df = df[['Year','Month','Day','T_max','T_min','Precip(mm)','T_ave','Precip(cm)','rh_ave']]
-		print("writing to %s" % fn)
-		df.to_csv(data_dir+'/'+fn,index=False)
+			# calculate precipitation in cm and ave relative humidity.
+			df["rh_ave"] = df[[CSV_HEADER_MAP['rhmax'], CSV_HEADER_MAP['rhmin']]].mean(axis=1)
+			df["Precip(cm)"] = df[CSV_HEADER_MAP["Precip(mm)"]].multiply(10)
+			df["T_ave"] = df[[CSV_HEADER_MAP['T_max'],CSV_HEADER_MAP['T_min']]].mean(axis=1)
+			# remove unwanted data
+			del df[CSV_HEADER_MAP["rhmax"]]
+			del df[CSV_HEADER_MAP["rhmin"]]
+			del df["yyyy-mm-dd"]
+			
+			# rename columns that we dont need to alter
+			df['Precip(mm)'] = df[CSV_HEADER_MAP['Precip(mm)']]
+			df = df[['Year','Month','Day','T_max','T_min','Precip(mm)','T_ave','Precip(cm)','rh_ave']]
+			print("writing to %s" % fn)
+			df.to_csv(data_dir+'/'+fn,index=False)
 main()
